8225bfb575bc8d9b20a3907d96b5cabb5eac74f2
gael <lehchibi.gael@gmail.com>
2019-09-06 17:06:44 +0200

    Replace Search

--- src/main/java/com/metaxiii/escalade/controller/path/Search.java
+++ src/main/java/com/metaxiii/escalade/controller/path/Search.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class Search {
     @RequestMapping("/search")
-    public String Search() {
+    public String search() {
         return "search";
     }
 }
