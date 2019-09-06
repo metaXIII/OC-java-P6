1593916e52625ce163c66a3fbbdaef90e8c1b3ba
gael <lehchibi.gael@gmail.com>
2019-09-06 12:10:07 +0200

    Add search front

---
+++ src/main/java/com/metaxiii/escalade/controller/path/Search.java
@@ -0,0 +1,12 @@
+package com.metaxiii.escalade.controller.path;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class Search {
+    @RequestMapping("/search")
+    public String Search() {
+        return "search";
+    }
+}
