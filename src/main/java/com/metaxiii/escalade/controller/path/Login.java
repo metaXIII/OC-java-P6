a6adbd0da97429c6c0962c6a7f9e8ee5cb9cbe19
gael <lehchibi.gael@gmail.com>
2019-09-10 11:30:26 +0200

    add login detail for connexion End of front visual Application responsive

--- src/main/java/com/metaxiii/escalade/controller/path/Login.java
+++ src/main/java/com/metaxiii/escalade/controller/path/Login.java
@@ -7,13 +7,16 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class Login extends AbstractState {
+
     @RequestMapping("/login/new-user")
-    public String create_user() {
+    public String create_user(Model model) {
+        model.addAttribute("user", "new");
         return "login";
     }
 
     @RequestMapping("/login/login")
-    public String connect_user() {
+    public String connect_user(Model model) {
+        model.addAttribute("user", "login");
         return "login";
     }
 }
