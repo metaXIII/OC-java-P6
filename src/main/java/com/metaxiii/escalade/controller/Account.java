f999bcb8f97577dec4573bafa3fccd3ba2e56a38
Meta <gael.lehchibi@outlook.com>
2019-12-09 15:57:30 +0100

    Remove ununcessary element

--- src/main/java/com/metaxiii/escalade/controller/Account.java
+++ src/main/java/com/metaxiii/escalade/controller/Account.java
@@ -13,11 +13,8 @@ import org.springframework.web.servlet.ModelAndView;
 public class Account {
     @RequestMapping("/account")
     public String account() {
-        // Pour récupèrer les infos users.
         User user = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
-        // Pour récupèrer les detail users (ip, session)
         WebAuthenticationDetails details = (WebAuthenticationDetails) SecurityContextHolder.getContext().getAuthentication().getDetails();
-        // Pour récupèrer les roles users
         String role = SecurityContextHolder.getContext().getAuthentication().getAuthorities().stream()
                 .map(GrantedAuthority::getAuthority)
                 .findFirst()
