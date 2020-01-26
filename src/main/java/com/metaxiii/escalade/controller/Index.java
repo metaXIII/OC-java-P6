0b63a4007c882a885da92030b915730edb35f67f
Meta <gael.lehchibi@outlook.com>
2020-01-26 14:07:28 +0100

    reformat code for index

--- src/main/java/com/metaxiii/escalade/controller/Index.java
+++ src/main/java/com/metaxiii/escalade/controller/Index.java
@@ -1,31 +1,28 @@
 package com.metaxiii.escalade.controller;
 
 import com.metaxiii.escalade.enums.Message;
-import com.metaxiii.escalade.model.User;
-import com.metaxiii.escalade.service.IUserService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 @RequiredArgsConstructor
-public class Index {
-    @Autowired
-    private IUserService userService;
+public class Index extends AbstractController {
 
-    @GetMapping("/")
-    public ModelAndView index() {
-        return new ModelAndView("index");
-    }
+	@GetMapping("/")
+	public ModelAndView index() {
+		return new ModelAndView("index");
+	}
 
-    @GetMapping("/welcome")
-    public ModelAndView welcome() {
-        User user = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
-        String msg = Message.BONJOUR.getMsg() + user.getUsername();
-        return new ModelAndView("index", "msg", msg);
-    }
+
+	@GetMapping("/welcome")
+	public ModelAndView welcome() {
+		ModelAndView modelAndView = new ModelAndView("index");
+		if (this.getUser() != null) {
+			String msg = Message.BONJOUR.getMsg() + getUser().getUsername();
+			modelAndView.addObject("msg", msg);
+		}
+		return modelAndView;
+	}
 }
