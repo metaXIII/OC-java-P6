5433af315eb6ca22cb46bc5c1aa952831d1c3732
Meta <gael.lehchibi@outlook.com>
2020-01-26 14:08:49 +0100

    Change configuration

--- src/main/java/com/metaxiii/escalade/config/WebSecurityConfig.java
+++ src/main/java/com/metaxiii/escalade/config/WebSecurityConfig.java
@@ -15,45 +15,43 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 @EnableWebSecurity
 @RequiredArgsConstructor
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
-	private final UserDetailsService userDetailsService;
-	private final PasswordEncoder passwordEncoder;
+    private final UserDetailsService userDetailsService;
+    private final PasswordEncoder    passwordEncoder;
 
-	@Override
-	protected void configure(HttpSecurity http) throws Exception {
-		http
-			.authorizeRequests()
-			.antMatchers("/", "/user/**", "/search", "/thanks", "/topo").permitAll()
-			.antMatchers("/css/**", "/js/**", "/img/**", "/vendor/**").permitAll()
-			.antMatchers(HttpMethod.GET, "/user/new-user", "/user/login").permitAll()
-			.antMatchers(HttpMethod.POST, "/user/new-user", "/user/login").permitAll()
-//			.antMatchers("/account").authenticated()
-//			.anyRequest().authenticated()
-			.anyRequest().permitAll()
-		.and()
-			.authenticationProvider(authProvider())
-			.formLogin()
-			.loginPage("/user/login") //
-			.failureUrl("/user/login?error")// L'url de la page de login
-			.defaultSuccessUrl("/welcome") // L'url de redirection après authentification
-			.usernameParameter("username") // Le name de l'input du form correspond au login
-			.passwordParameter("password") //// Le name de l'input du form correspond au password
-		.and()
-			.logout()
-			.invalidateHttpSession(true)
-			.logoutUrl("/user/logout")
-			.logoutSuccessUrl("/user/login?logout")
-		.and()
-			.csrf()
-			.disable()
-		;
-	}
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .antMatchers("/", "/user/**", "/search", "/thanks", "/topo", "/contact",
+                        "/details-site/**", "/css/**", "/js/**", "/img/**", "/vendor/**").permitAll()
+                .antMatchers(HttpMethod.POST, "/user/new-user", "/user/login").permitAll()
+                .antMatchers("/account").authenticated()
+                .anyRequest().authenticated()
+                .and()
+                .authenticationProvider(authProvider())
+                .formLogin()
+                .loginPage("/user/login")
+                .failureUrl("/user/login?error")
+                .defaultSuccessUrl("/welcome")
+                .usernameParameter("username")
+                .passwordParameter("password")
+                .and()
+                .logout()
+                .invalidateHttpSession(true)
+                .logoutUrl("/user/logout")
+                .logoutSuccessUrl("/user/login?logout")
+                .and()
+                .csrf()
+                .disable()
+        ;
+    }
 
 
-	@Bean
-	public DaoAuthenticationProvider authProvider() {
-		DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
-		authProvider.setUserDetailsService(userDetailsService);
-		authProvider.setPasswordEncoder(passwordEncoder);
-		return authProvider;
-	}
+    @Bean
+    public DaoAuthenticationProvider authProvider() {
+        DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+        authProvider.setUserDetailsService(userDetailsService);
+        authProvider.setPasswordEncoder(passwordEncoder);
+        return authProvider;
+    }
 }
