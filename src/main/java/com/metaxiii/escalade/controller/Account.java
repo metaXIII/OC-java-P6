9d629d828c98f23c8b9abb40c1d38820640f170a
Meta <gael.lehchibi@outlook.com>
2020-02-02 17:13:33 +0100

    Reformat, SonarLint and add unsuscribe function, end of fonctionnality

--- src/main/java/com/metaxiii/escalade/controller/Account.java
+++ src/main/java/com/metaxiii/escalade/controller/Account.java
@@ -4,58 +4,50 @@ import com.metaxiii.escalade.dto.SiteDto;
 import com.metaxiii.escalade.dto.TopoDto;
 import com.metaxiii.escalade.enums.Message;
 import com.metaxiii.escalade.model.Reservation;
-import com.metaxiii.escalade.model.User;
 import com.metaxiii.escalade.service.*;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.context.SecurityContextHolder;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
-import org.springframework.validation.Errors;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
 import java.text.ParseException;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 @Controller
+@RequiredArgsConstructor
+@Slf4j
 public class Account extends AbstractController {
 
-    @Autowired
-    private IDepartementService departementService;
+    private final IDepartementService departementService;
 
-    @Autowired
-    private ISiteService siteService;
+    private final ISiteService siteService;
 
-    @Autowired
-    private ISecteurService secteurService;
+    private final ISecteurService secteurService;
 
-    @Autowired
-    private IUserService userService;
+    private final IReservationService reservationService;
 
-    @Autowired
-    private IReservationService reservationService;
-
-    @Autowired
-    private ITopoService topoService;
+    private final ITopoService topoService;
 
     @GetMapping("/account")
     public ModelAndView account() {
-        User         user         = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
         ModelAndView modelAndView = new ModelAndView("account");
-        modelAndView.addObject("reservations", reservationService.findAllReservationByUserId(user.getId()));
+        modelAndView.addObject("reservations", reservationService.findAllReservationByUserId(getUser().getId()));
+        modelAndView.addObject("topoList", topoService.findAllByUserId(getUser().getId()));
         return modelAndView;
     }
 
+    @GetMapping("/account/reservations/unsuscribe/{id}")
+    @ResponseBody
+    public ModelAndView unsubscribeReservation(@PathVariable String id) {
+        reservationService.completeReservation(Long.parseLong(id));
+        return new ModelAndView("redirect:/account");
+    }
+
+
     @GetMapping("/account/new-site")
-    public ModelAndView new_site(@ModelAttribute("site") SiteDto siteDto,
-                                 BindingResult result, WebRequest request, Errors errors, Model model) {
+    public ModelAndView newSite(@ModelAttribute("site") SiteDto siteDto) {
         ModelAndView modelAndView = new ModelAndView("site");
         modelAndView.addObject("form", "new");
         modelAndView.addObject("secteurList", secteurService.findAllSecteur());
@@ -63,30 +55,36 @@ public class Account extends AbstractController {
         return modelAndView;
     }
 
-
     @PostMapping("/account/new-site")
     @ResponseBody
-    public ModelAndView save_site(@ModelAttribute("site") SiteDto siteDto,
-                                  BindingResult result, WebRequest request, Errors errors, Model model) {
-        Map<String, Object> datas = new HashMap<>();
-        datas.put("form", "new");
-        datas.put("departement_list", departementService.findAllDepartement());
+    public ModelAndView saveSite(@ModelAttribute("site") SiteDto siteDto) {
         try {
-            User   user     = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
-            int    id       = (int) user.getId();
-            String redirect = "redirect:/details-site/" + siteService.save(siteDto, id).getId();
+            String redirect = "redirect:/details-site/" + siteService.save(siteDto, getUser().getId()).getId();
             return new ModelAndView(redirect, "msg", Message.SAVE_SITE.getMsg());
         } catch (Exception e) {
-            datas.put("msg", e.getMessage());
-            return new ModelAndView("site", "datas", datas);
+            return newSite(siteDto);
         }
     }
 
+    @GetMapping("/account/new-topo")
+    public ModelAndView newTopo(Model model) {
+        ModelAndView modelAndView = new ModelAndView("topo");
+        modelAndView.addObject("show", true);
+        modelAndView.addObject("departement_list", departementService.findAllDepartement());
+        return modelAndView;
+    }
+
+    @PostMapping("/account/new-topo")
+    public ModelAndView saveTopo(Model model, @ModelAttribute("site") TopoDto topoDto) throws ParseException {
+        topoService.save(topoDto);
+        String msg = "Merci pour votre participation";
+        return new ModelAndView("index", "msg", msg);
+    }
 
 
     @GetMapping("/account/reservations")
     @ResponseBody
-    public ModelAndView getReservationList(Model model) {
+    public ModelAndView getReservationList() {
         ModelAndView      modelAndView = new ModelAndView("account");
         List<Reservation> findAll      = reservationService.findAllReservationByUserId(getUser().getId());
         modelAndView.addObject("show", true);
