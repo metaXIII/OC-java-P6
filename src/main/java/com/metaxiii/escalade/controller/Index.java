2f1f549240efee06c2a2aecd857fd4b10420486c
Meta <gael.lehchibi@outlook.com>
2020-02-06 12:24:53 +0100

    404 error

--- src/main/java/com/metaxiii/escalade/controller/Index.java
+++ src/main/java/com/metaxiii/escalade/controller/Index.java
@@ -2,27 +2,42 @@ package com.metaxiii.escalade.controller;
 
 import com.metaxiii.escalade.enums.Message;
 import lombok.RequiredArgsConstructor;
+import org.springframework.boot.web.servlet.error.ErrorController;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 @RequiredArgsConstructor
-public class Index extends AbstractController {
+public class Index extends AbstractController implements ErrorController {
 
-	@GetMapping("/")
-	public ModelAndView index() {
-		return new ModelAndView("index");
-	}
+    @GetMapping("/")
+    public ModelAndView index() {
+        return new ModelAndView("index");
+    }
 
 
-	@GetMapping("/welcome")
-	public ModelAndView welcome() {
-		ModelAndView modelAndView = new ModelAndView("index");
-		if (this.getUser() != null) {
-			String msg = Message.BONJOUR.getMsg() + getUser().getUsername();
-			modelAndView.addObject("msg", msg);
-		}
-		return modelAndView;
-	}
+    @GetMapping("/welcome")
+    public ModelAndView welcome() {
+        ModelAndView modelAndView = new ModelAndView("index");
+        if (this.getUser() != null) {
+            String msg = Message.BONJOUR.getMsg() + getUser().getUsername();
+            modelAndView.addObject("msg", msg);
+        }
+        return modelAndView;
+    }
+
+    private static final String PATH = "/error";
+
+    @RequestMapping(value = PATH)
+    public String error() {
+        return "404";
+    }
+
+    @Override
+    public String getErrorPath() {
+        return "404";
+    }
+
 }
